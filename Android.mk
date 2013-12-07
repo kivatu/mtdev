2402247f3039da73bbe0310bb2e98c9843bf8d14
midesimo <@>
2013-12-06 16:51:14 -0800

    First port of mtdev; new (working ;)) Android.mk.

--- Android.mk
+++ Android.mk
@@ -1,44 +1,24 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
+# Build libmtdev
 include $(CLEAR_VARS)
+LOCAL_SRC_FILES := \
+	src/caps.c \
+	src/core.c \
+	src/iobuf.c \
+	src/match.c \
+	src/match_four.c
+#LOCAL_CFLAGS := -DSASR
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+LOCAL_MODULE := libmtdev
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
 
-MTDEV_TOP := $(LOCAL_PATH)
-
-MTDEV_BUILT_MAKEFILES := \
-	$(MTDEV_TOP)/src/Android.mk
-
-mtdev-configure:
-	cd $(MTDEV_TOP) && autoreconf -fiv
-	cd $(MTDEV_TOP) && \
-		CC="$(CONFIGURE_CC)" \
-		CFLAGS="$(CONFIGURE_CFLAGS)" \
-		LD=$(TARGET_LD) \
-		LDFLAGS="$(CONFIGURE_LDFLAGS)" \
-		CPP=$(CONFIGURE_CPP) \
-		CPPFLAGS="$(CONFIGURE_CPPFLAGS)" \
-		PKG_CONFIG_LIBDIR=$(CONFIGURE_PKG_CONFIG_LIBDIR) \
-		PKG_CONFIG_TOP_BUILD_DIR=$(PKG_CONFIG_TOP_BUILD_DIR) \
-		./configure --host=arm-linux-androideabi \
-		--prefix /system
-	rm -f $(MTDEV_BUILT_MAKEFILES)
-	@for file in $(MTDEV_BUILT_MAKEFILES); do \
-		echo "make -C $$(dirname $$file) $$(basename $$file)" ; \
-		make -C $$(dirname $$file) $$(basename $$file) ; \
-	done
-
-mtdev-reset:
-	cd $(MTDEV_TOP) && \
-	git clean -qdxf && \
-	git reset --hard HEAD
-
-mtdev-clean:
-
-.PHONY: mtdev-configure mtdev-clean mtdev-reset
-
-CONFIGURE_TARGETS += mtdev-configure
-CONFIGURE_RESET_TARGETS += mtdev-reset
-AGGREGATE_CLEAN_TARGETS += mtdev-clean
-CONFIGURE_PKG_CONFIG_LIBDIR := $(CONFIGURE_PKG_CONFIG_LIBDIR):$(MTDEV_TOP)
-
--include $(MTDEV_BUILT_MAKEFILES)
-
+# Build mtdev-test
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := test/mtdev-test.c
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := libmtdev
+LOCAL_MODULE := mtdev-test
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
